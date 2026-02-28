3575ed716d4e4f6ed542f9c0c49b9277c1a37a52
AmaseCocoa <cocoa@amase.cc>
2026-02-28 15:52:48 +0900

    Update profiles.rs

--- src/bin/credm/oauth/profiles.rs
+++ src/bin/credm/oauth/profiles.rs
@@ -9,9 +9,8 @@ struct OAuth2Profile {
 static OAUTH2_PROFILES: LazyLock<HashMap<&'static str, OAuth2Profile>> = LazyLock::new(|| {
     let mut m = HashMap::new();
     m.insert("github.com", OAuth2Profile {
-        
         client_id: "Ov23liM4hSM09bDhwmOG".to_string(),
-        client_secret: Some("eccb1988f224f0ef54bd78f6b4691f9e854f3449".to_string()),
+        client_secret: Some("eccb1988f224f0ef54bd78f6b4691f9e854f3449".to_string()), // this secret is not secret, required for github oauth
         scope: ["repo".to_string(), "gist".to_string(), "workflow".to_string()].to_vec()
     });
     m
